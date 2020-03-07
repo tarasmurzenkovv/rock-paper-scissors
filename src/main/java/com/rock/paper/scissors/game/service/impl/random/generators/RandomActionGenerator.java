87ce8c3393080ba29a20ac49462baf18fdb7e3f7
unknown <murzenkovtn@gmail.com>
2020-03-07 22:23:39 +0200

    global applcation refactoring: interface to Supplier, static factories, test clean up, more assertions

--- src/main/java/com/rock/paper/scissors/game/service/impl/random/generators/RandomActionGenerator.java
+++ src/main/java/com/rock/paper/scissors/game/service/impl/random/generators/RandomActionGenerator.java
@@ -6,10 +6,12 @@ import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Supplier;
 
 public class RandomActionGenerator implements Supplier<PlayerAction> {
+    private static final int LOW_BOUND = 1;
+    private static final int UPPER_BOUND = 4;
 
     @Override
     public PlayerAction get() {
-        int ordinalNumberOfPlayerAction = ThreadLocalRandom.current().nextInt(1, 4);
+        var ordinalNumberOfPlayerAction = ThreadLocalRandom.current().nextInt(LOW_BOUND, UPPER_BOUND);
         return PlayerAction.from(ordinalNumberOfPlayerAction);
     }
 }
