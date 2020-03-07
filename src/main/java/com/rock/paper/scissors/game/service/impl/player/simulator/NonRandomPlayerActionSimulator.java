255220ab808f5a488514b5a5947ea370983efef2
unknown <murzenkovtn@gmail.com>
2020-03-07 22:17:45 +0200

    PlayerActionSimulator: refactored by expsing dependency thorugh constructor

--- src/main/java/com/rock/paper/scissors/game/service/impl/player/simulator/NonRandomPlayerActionSimulator.java
+++ src/main/java/com/rock/paper/scissors/game/service/impl/player/simulator/NonRandomPlayerActionSimulator.java
@@ -1,16 +1,18 @@
 package com.rock.paper.scissors.game.service.impl.player.simulator;
 
 import com.rock.paper.scissors.game.model.PlayerAction;
+import com.rock.paper.scissors.game.service.ActionGenerator;
 import com.rock.paper.scissors.game.service.PlayerActionSimulator;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public class NonRandomPlayerActionSimulator implements PlayerActionSimulator {
     private final String playerName;
+    private final ActionGenerator actionGenerator;
 
     @Override
     public PlayerAction generate() {
-        return PlayerAction.ROCK;
+        return actionGenerator.generate();
     }
 
     @Override
--- src/test/java/com/rock/paper/scissors/game/service/player/simulator/NonRandomPlayerActionSimulatorTest.java
+++ src/test/java/com/rock/paper/scissors/game/service/player/simulator/NonRandomPlayerActionSimulatorTest.java
@@ -3,11 +3,12 @@ package com.rock.paper.scissors.game.service.player.simulator;
 import com.rock.paper.scissors.game.model.PlayerAction;
 import com.rock.paper.scissors.game.service.PlayerActionSimulator;
 import com.rock.paper.scissors.game.service.impl.player.simulator.NonRandomPlayerActionSimulator;
+import com.rock.paper.scissors.game.service.impl.random.generators.NonRandomActionGenerator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class NonRandomPlayerActionSimulatorTest {
-    PlayerActionSimulator sut = new NonRandomPlayerActionSimulator("Player 1");
+    PlayerActionSimulator sut = new NonRandomPlayerActionSimulator("Player 1", new NonRandomActionGenerator());
 
     @Test
     public void shouldReturnRockActionForNonRandomActionGenerator() {
