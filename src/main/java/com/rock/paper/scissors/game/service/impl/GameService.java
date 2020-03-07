c60a48b079d931912bd4901198b57d208da684f6
unknown <murzenkovtn@gmail.com>
2020-03-07 23:27:06 +0200

    GameService: fixed failing test for rock and scissors

--- src/main/java/com/rock/paper/scissors/game/service/impl/GameService.java
+++ src/main/java/com/rock/paper/scissors/game/service/impl/GameService.java
@@ -1,6 +1,7 @@
 package com.rock.paper.scissors.game.service.impl;
 
 import com.rock.paper.scissors.game.model.GameState;
+import com.rock.paper.scissors.game.model.PlayerAction;
 import com.rock.paper.scissors.game.service.Game;
 import com.rock.paper.scissors.game.service.PlayerActionSimulator;
 
@@ -11,6 +12,8 @@ public class GameService implements Game {
         var actionOfSecondPlayer = secondPlayer.generate();
         if (actionOfFirstPlayer == actionOfSecondPlayer) {
             return GameState.DRAW;
+        } else if (actionOfSecondPlayer == PlayerAction.SCISSORS) {
+            return GameState.WIN;
         }
         return null;
     }
